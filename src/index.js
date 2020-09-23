1078e323318d1742c251a7faf89725145fc6bfec
mumu111111 <2646709035@qq.com>
2020-09-24 07:42:26 +0800

    切换输出 x o

--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-const cells = [
-    [1, null, null],
-    [null, null, null],
-    [null, null, null],
-]
+
 const Cell = function (props) {
-    return  (
-        <div className="cell">
+    // const onClickCell = function () {
+    // props.text = 'x'  //不能修改 text 因为他是一个只读属性
+    // }
+    return (
+        <div className="cell" onClick={props.onClick}>
             {props.text}
         </div>
     )
 }
+// 传递方式  父组件 赋值参数方式  传递给 子组件 子接收 
 
 const Chessboard = function () {
+    const [cells, setCells] = React.useState([
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ])
+    const [n, setN] = React.useState(0)
+    const onClickCell = (row, col) => {
+        // 能够操作 数组  但是数组也没在 这个组件中   
+        // 原则 只有是自己的变量才能修改  er当前是变量cells 
+        // onClick={() => onClickCell(row, col)} 避免没点击前 就已经执行
+        const copy = JSON.parse(JSON.stringify(cells))
+        copy[row][col] = n % 2 === 0 ? 'X' : 'O'
+        setN(n + 1)
+        setCells(copy)
+    }
+    // 更新后的 cells 便利后 将新的 item  给到 子组件 
     return (
         <div>
-            {cells.map(items=> <div className="row">
-                {items.map(item => <div className="col">
-                    <Cell text={item} />
+            {cells.map((items, row) => <div className="row">
+                {items.map((item, col) => <div className="col">
+                    <Cell text={item} onClick={() => onClickCell(row, col)} />
                 </div>)}
             </div>)}
         </div>
     )
 }
-// export default Chessboard;
 
-
+// 加入渲染到页面
 ReactDOM.render(<div>
     <Chessboard />
 </div>, document.getElementById('root'))
